677d16316cace9c95a98e1605de587cf4f5dd93c
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-07-16 12:15:02 +0600

    Deploy Project done

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,17 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
-    reducerPath:'api',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'http://192.168.10.156:4040/api/v1',
-        prepareHeaders: (headers, { getState }) => {
-            const {token} = getState().auth
-            if (token) {
-              headers.set('authorization', `Bearer ${token}`)
-            }
-            return headers
-          },
-    }),
-    tagTypes: ['Withdraw'],
-    endpoints:()=>({
-    })
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://3c8c-103-43-151-152.ngrok-free.app/api/v1',
+    prepareHeaders: (headers, { getState }) => {
+      const { token } = getState().auth
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  tagTypes: ['Withdraw'],
+  endpoints: () => ({
+  })
 })
