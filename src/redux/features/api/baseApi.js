577214fa109b64dd17e5fe94644cbf6eb1d4f402
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-07-25 10:37:07 +0600

    [ADDED]

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -3,7 +3,8 @@ export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     // baseUrl: 'https://3c8c-103-43-151-152.ngrok-free.app/api/v1',
-    baseUrl: 'http://192.168.10.156:4040/api/v1',
+    // baseUrl: 'http://192.168.10.156:4040/api/v1',
+    baseUrl: 'http://147.182.133.11:4040/api/v1',
     prepareHeaders: (headers, { getState }) => {
       const { token } = getState().auth
       if (token) {
